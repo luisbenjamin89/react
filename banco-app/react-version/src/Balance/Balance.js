05abbcba2557fc2c0f74f36e8440007b7878d23f
benjamin gutierrez <benjamingutiterrez89@gmail.com>
2024-02-19 11:51:43 +0100

    agregando los componentes y primeras funcioens al proyecto banco en react

---
+++ banco-app/react-version/src/Balance/Balance.js
@@ -0,0 +1,22 @@
+
+import './Balance.css'
+
+function Balance(prods) {
+  const { movements } = prods
+  const balance =movements.reduce((acc,mov) => acc+mov,0)
+  return (
+    <>
+      <div className="balance">
+        <div>
+          <p className="balance__label">Current balance</p>
+          <p className="balance__date">
+            As of <span className="date">05/03/2037</span>
+          </p>
+        </div>
+        <p className="balance__value">{balance}</p>
+      </div>
+    </>
+  )
+}
+
+export default Balance
